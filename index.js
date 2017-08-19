1593785bb1556e4256c5061432c8790dfebfb178
Jossua <weyzenpro@outlook.com>
2017-08-19 12:48:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,4 +18,36 @@ bot.on('message', message => {
     // On pourrait catch l'erreur autrement ici (l'utilisateur a peut être désactivé les MP)
   })
 
+bot.on('message', message => {
+
+  if (message.content.startsWith('!play')) {
+    // On récupère le premier channel audio du serveur
+    let voiceChannel = message.guild.channels
+      .filter(function (channel) { return channel.type === 'voice' })
+      .first()
+    // On récupère les arguments de la commande 
+    // il faudrait utiliser une expression régulière pour valider le lien youtube
+    let args = message.content.split(' ')
+    // On rejoint le channel audio
+    voiceChannel
+      .join()
+      .then(function (connection) {
+        // On démarre un stream à partir de la vidéo youtube
+        let stream = YoutubeStream(args[1])
+        stream.on('error', function () {
+          message.reply("Je n'ai pas réussi à lire cette vidéo :(")
+          connection.disconnect()
+        })
+        // On envoie le stream au channel audio
+        // Il faudrait ici éviter les superpositions (envoie de plusieurs vidéo en même temps)
+        connection
+          .playStream(stream)
+          .on('end', function () {
+            connection.disconnect()
+          })
+      })
+  }
+
+})
+
 bot.login('')
